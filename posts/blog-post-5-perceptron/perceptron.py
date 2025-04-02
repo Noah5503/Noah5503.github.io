83adbb7d5781db6d4cd9edcce0940cdeec0faf1c
Noah Price <nprice@middlebury.edu>
2025-04-02 13:33:18 -0400

    perceptron.py

---
+++ posts/blog-post-5-perceptron/perceptron.py
@@ -0,0 +1,111 @@
+import torch
+
+class LinearModel:
+
+    def __init__(self):
+        self.w = None 
+
+    def score(self, X):
+        """
+        Compute the scores for each data point in the feature matrix X. 
+        The formula for the ith entry of s is s[i] = <self.w, x[i]>. 
+
+        If self.w currently has value None, then it is necessary to first initialize self.w to a random value. 
+
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+
+        RETURNS: 
+            s torch.Tensor: vector of scores. s.size() = (n,)
+        """
+        if self.w is None: 
+            self.w = torch.rand((X.size()[1]))
+        return X @ self.w 
+
+    def predict(self, X):
+        """
+        Compute the predictions for each data point in the feature matrix X. The prediction for the ith data point is either 0 or 1. 
+
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+
+        RETURNS: 
+            y_hat, torch.Tensor: vector predictions in {0.0, 1.0}. y_hat.size() = (n,)
+        """
+        scores = self.score(X)
+        return (scores >= 0)
+
+class Perceptron(LinearModel):
+
+    def loss(self, X, y):
+        """
+        Compute the misclassification rate. A point i is classified correctly if it holds that s_i*y_i_ > 0, where y_i_ is the *modified label* that has values in {-1, 1} (rather than {0, 1}). 
+
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+
+            y, torch.Tensor: the target vector.  y.size() = (n,). The possible labels for y are {0, 1}
+        
+        RETURNS: 
+            misclassified.mean(), which is the average rate of misclassification with the current X and w.
+        """
+        scores = self.score(X)
+        y_ = 2*y - 1 # Convert to {-1, 1}
+        misclassified = (scores * y_ < 0).float()
+        return misclassified.mean()
+
+    def grad(self, X, y):
+        """
+        Compute the change in weight vector. The change is equal to -1[s_i(2*y_i - 1) <= 0](2*y_i - 1)*x_i.
+
+        ARGUMENTS:
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+
+            y, torch.Tensor: the target vector.  y.size() = (n,). The possible labels for y are {0, 1}
+
+        RETURNS:
+            The vector of weight changes to be applied to w in Perceptron.step().
+
+        """
+        scores = self.score(X)
+        y_ = 2*y - 1
+        misclassified = (scores * y_ < 0).float().unsqueeze(1)
+        return torch.sum(misclassified * (y_.unsqueeze(1) * X), dim=0)
+
+class PerceptronOptimizer:
+
+    def __init__(self, model):
+        self.model = model 
+    
+    def step(self, X, y):
+        """
+        Compute one step of the perceptron update using the feature matrix X 
+        and target vector y. 
+
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+
+            y, torch.Tensor: the target vector.  y.size() = (n,). The possible labels for y are {0, 1}
+
+        RETURNS:
+            current_loss, which is the loss value for X and w before adjustment
+        """
+        current_loss = self.model.loss(X, y)
+        gradient = self.model.grad(X, y)
+        self.model.w += gradient
+        return current_loss
